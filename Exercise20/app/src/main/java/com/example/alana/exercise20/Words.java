3805051106d31ca349524e44af60d0b4a7991420
TianJingwen <jingwen8299@126.com>
2016-12-06 20:14:20 +0800

    Exercise20
    SQLite数据库使用

---
+++ Exercise20/app/src/main/java/com/example/alana/exercise20/Words.java
@@ -0,0 +1,20 @@
+package com.example.alana.exercise20;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Alana on 2016/9/27.
+ */
+
+public class Words {
+    public Words() {
+    }
+
+    public static abstract class Word implements BaseColumns {
+        public static final String TABLE_NAME="words";
+        public static final String COLUMN_NAME_WORD="word";//列：单词
+        public static final String COLUMN_NAME_MEANING="meaning";//列：单词含义
+        public static final String COLUMN_NAME_SAMPLE="sample";//单词示例
+    }
+}
+
