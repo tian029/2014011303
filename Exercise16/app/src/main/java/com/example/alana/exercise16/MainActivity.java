6d9f79784f2ee62374eedde952ff22f0ee24e542
TianJingwen <jingwen8299@126.com>
2016-12-06 20:09:25 +0800

    Exercise16
    进一步熟悉Intent Filter

---
+++ Exercise16/app/src/main/java/com/example/alana/exercise16/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.alana.exercise16;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("schemodemo://edu.bistu/path"));
+        startActivity(intent);
+    }
+}
